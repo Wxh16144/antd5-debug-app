47acaa14043a8d3ce856cfbb1c925c9c9a66a0c2
wuxh <wxh1220@gmail.com>
2024-05-11 14:26:19 +0800

    chore: add ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import process from 'node:process'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.MY_SITE_BASE ?? '/',
   plugins: [
     react(),
     Pages({
